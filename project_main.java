18b3ed0e9112a79d2a2aacf00c33635bf1401288
spencergamero23 <bluerayman23@gmail.com>
2025-06-03 02:23:27 -0700

    Created some classes as well as added a plan!

--- project_main.java
+++ project_main.java
@@ -4,3 +4,11 @@ public class project_main {
         System.out.println("Hello world");
     }
 }
+
+// What does this task manager do?
+// Prompt: Task Scheduler with Calendar: Create a task scheduler with an integrated calendar that allows users to manage events and tasks.
+// Our project is the task scheduler!
+// It includes: an integrated calendar. What does the calendar do? It allows user to manage events and tasks!
+// Two classes we would need is:
+//task: name, date, description, priority, and status
+//task manager: adds tasks, remove tasks, retrieves tasks by date
