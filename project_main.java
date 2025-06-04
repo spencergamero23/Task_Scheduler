0ab1bf616f97f656c4f62a16f2e0668f9077b5f7
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2025-06-03 21:10:56 -0700

    Added a basic command line input out put

--- project_main.java
+++ project_main.java
@@ -1,10 +1,6 @@
+import  java.util.Scanner;
+
 public class project_main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
-
 // What does this task manager do?
 // Prompt: Task Scheduler with Calendar: Create a task scheduler with an integrated calendar that allows users to manage events and tasks.
 // Our project is the task scheduler!
@@ -12,3 +8,37 @@ public class project_main {
 // Two classes we would need is:
 //task: name, date, description, priority, and status
 //task manager: adds tasks, remove tasks, retrieves tasks by date
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String input;
+
+        Systesm.out.println("Hello and welcome to a task manager!")
+        While (input == "4" || input == "Exit"){
+            System.out.println("Menu:")
+            System.out.println("1. Add Task")
+            System.out.println("2. Remove Task")
+            System.out.println("3. View Tasks")
+            System.out.println("4. Exit")
+
+            input = myObj.nextLine();
+
+            if (input == "1" || input == "Add Task"){
+
+            }
+            else if (input == "2" || input == "Remove Task"){
+
+            }
+            else if (input == "3" || input == "View Tasks"){
+
+            }
+
+            else{
+                System.out.println("ERROR: input one of the choices above.")
+            }
+        }
+        
+
+        
+    }
+}
